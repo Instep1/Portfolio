845a5bbf2f402026e610f66575e4583f5d1cbc91
Maksim <lvutinwork@gmail.com>
2025-02-09 10:13:04 +0300

    feat: added section #5 with price list

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,9 @@ const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       closeElem = document.querySelector('.menu__close');
 
+const counters = document.querySelectorAll('.skills__item_score-info-percent'),
+      lines = document.querySelectorAll('.skills__item_score-bar .skills__item_score-bar-fill');
+
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
@@ -10,10 +13,6 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
-const counters = document.querySelectorAll('.skills__item_score-info-percent'),
-      lines = document.querySelectorAll('.skills__item_score-bar .skills__item_score-bar-fill');
-
 counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
-    
 })
